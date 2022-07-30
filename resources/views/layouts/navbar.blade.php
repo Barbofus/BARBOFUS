8f831b846996cf383ce40dfb3cc268a67e94f104
CharlyMollard <charly.mollard@gmail.com>
2022-07-30 14:19:06 +0200

    Tets avec la navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,12 +1,22 @@
 <div class="text-2xl justify-center pl-5 pt-2 pb-3 border-b">
     <ul class="flex">
-        <li><a href="{{ route('index') }}" class="pr-4 hover:font-bold focus:font-bold focus:hover:text-gray-600">Accueil</a></li>
-        <li><a href="{{ route('about') }}" class=" pr-4 hover:font-bold focus:font-bold focus:hover:text-gray-600">About</a></li>
+        <li class="{{ Route::is('index') ? "active" : "" }}"><a href="{{ route('index') }}" class="pr-4 hover:font-bold">Accueil</a></li>
+        <li class="{{ Route::is('about') ? "active" : "" }}"><a href="{{ route('about') }}" class=" pr-4 hover:font-bold">About</a></li>
 
         @guest
-            <li class="active"><a href="{{ route('login') }}" class="pr-4 hover:font-bold focus:font-bold focus:hover:text-gray-600 active:text-red-600">Login</a></li>
-            <li><a href="#" class="hover:font-bold focus:font-bold focus:hover:text-gray-600">Register</a></li>
+            <li class="{{ Route::is('login') ? "active" : "" }}"><a href="{{ route('login') }}" class="pr-4 hover:font-bold">Login</a></li>
+            <li><a href="#" class="hover:font-bold">Register</a></li>
         @endguest
+
+        <style>
+            li.active{
+                font-weight : bold;
+            }
+            
+            li.active:hover{
+                color : #5e5e5e;
+            }
+        </style>
 
         @auth
             <li><a href="#">Log out</a></li>
