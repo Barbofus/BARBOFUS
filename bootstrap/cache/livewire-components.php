70e91825a9854623f32305a2e329f81702f26dd5
CharlyMollard <charly.mollard@gmail.com>
2023-02-28 21:43:47 +0100

    debug cache

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,5 @@
+<?php return array (
+  'filter-builds' => 'App\\Http\\Livewire\\FilterBuilds',
+  'user-page.filter-builds' => 'App\\Http\\Livewire\\UserPage\\FilterBuilds',
+  'user-page.users-list' => 'App\\Http\\Livewire\\UserPage\\UsersList',
+);
