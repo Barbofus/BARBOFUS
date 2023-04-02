d451553f969b5696b226f92622ca9f8a28dd955e
CharlyMollard <charly.mollard@gmail.com>
2023-04-02 13:24:10 +0200

    Seeder / Factory des Skins

--- database/factories/SkinFactory.php
+++ database/factories/SkinFactory.php
@@ -2,6 +2,13 @@
 
 namespace Database\Factories;
 
+use App\Models\DofusItemCloak;
+use App\Models\DofusItemCostume;
+use App\Models\DofusItemHat;
+use App\Models\DofusItemPet;
+use App\Models\DofusItemShield;
+use App\Models\Race;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +24,30 @@ class SkinFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'dofus_item_hat_id' => rand(0,4) ? DofusItemHat::all()->random()->id : null,
+            'dofus_item_cloak_id' => rand(0,4) ? DofusItemCloak::all()->random()->id : null,
+            'dofus_item_shield_id' => rand(0,4) ? DofusItemShield::all()->random()->id : null,
+            'dofus_item_pet_id' => rand(0,4) ? DofusItemPet::all()->random()->id : null,
+            'dofus_item_costume_id' => rand(0,4) ? DofusItemCostume::all()->random()->id : null,
+            'face' => rand(1, 8),
+            'image_path' => 'images/skins/Skin' . rand(1, 6) . '.png',
+            'gender' => rand(0, 1) ? 'Femme' : 'Homme',
+            'color_skin' => $this->GetRandomColor(),
+            'color_hair' => $this->GetRandomColor(),
+            'color_cloth_1' => $this->GetRandomColor(),
+            'color_cloth_2' => $this->GetRandomColor(),
+            'color_cloth_3' => $this->GetRandomColor(),
+            'user_id' => User::all()->random()->id,
+            'race_id' => Race::all()->random()->id,
+            'status' => 'Posted',
         ];
+    }
+
+    protected function GetRandomColor() : string {
+        return $this->GetRandomColorPart() . $this->GetRandomColorPart() . $this->GetRandomColorPart();
+    }
+
+    protected function GetRandomColorPart() : string {
+        return str_pad( dechex( mt_rand( 0, 255 ) ), 2, '0', STR_PAD_LEFT);
     }
 }
--- database/seeders/SkinSeeder.php
+++ database/seeders/SkinSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Skin;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,6 @@ class SkinSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Skin::factory(50)->create();
     }
 }
