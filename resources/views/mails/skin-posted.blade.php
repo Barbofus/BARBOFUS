bd8c2428da6e2dd31f6694a3d3d76f055ad6dcdc
CharlyMollard <charly.mollard@gmail.com>
2023-04-02 11:56:28 +0200

    Envoi de mails finis

--- resources/views/mails/skin-posted.blade.php
+++ resources/views/mails/skin-posted.blade.php
@@ -2,7 +2,7 @@
 <x-mail::message>
 # Skin posté
 
-Salut {{ $user->name }},<br>
+Salut <span class="italic">{{ $user->name }}</span>,<br>
 <span>Ton skin ***ID#{{ $skin->id }}*** en **{{ $skin->race->name }}** à été validé par un membre du Staff.</span>
 
 <div style="text-align: center;"><img src="data:image/png;base64,{{ base64_encode(file_get_contents(public_path('storage/' . $skin->image_path ))) }}" alt="Image du skin {{ $skin->id }}"></div>
--- resources/views/mails/skin-refused.blade.php
+++ resources/views/mails/skin-refused.blade.php
@@ -2,10 +2,10 @@
 <x-mail::message>
 # Skin refusé
 
-Salut {{ $user->name }},<br>
+Salut <span class="italic">{{ $user->name }}</span>,<br>
 <span>Ton skin ***ID#{{ $skin->id }}*** en **{{ $skin->race->name }}** à été refusé par un membre du Staff.</span>
 
-<div style="text-align: center;"><img src="data:image/png;base64,{{ base64_encode(file_get_contents(public_path('storage/' . $skin->image_path ))) }}" alt="Image du skin {{ $skin->id }}"></div>
+<div style="text-align: center;"><img class="refused-img" src="data:image/png;base64,{{ base64_encode(file_get_contents(public_path('storage/' . $skin->image_path ))) }}" alt="Image du skin {{ $skin->id }}"></div>
 
 @if($skin->refused_reason)
 <x-mail::panel>
