e5771fe024550f55a847d72d4cf82c5ea7ddc347
CharlyMollard <charly.mollard@gmail.com>
2023-06-20 22:08:20 +0200

    Début design 'mon-compte' + design admin-panel.blade.php

--- resources/views/components/user-page-navbar.blade.php
+++ resources/views/components/user-page-navbar.blade.php
@@ -1,45 +1,86 @@
-<div>
-    <h1 class=" text-5xl font-bold text-center mt-[50px]">Bienvenue sur ton espace compte !</h1>
-    <h2 class=" text-3xl italic text-center mt-[10px]">Je suis encore en construction, mais... on se reverra bientôt</h2>
+<div class="flex justify-center mt-36 mb-10">
 
-    <div class="flex justify-center mt-10 mb-10">
+    {{-- Création de variable de class pour éviter les copier coller avec AlpineJS --}}
+    <div
+        class="w-[80%] flex overflow-hidden" x-cloak
+        x-data="{
+            initButtonClass: 'border-b-2 text-inactiveText border-primary px-6 h-[65px] flex gap-x-2 items-center text-left text-xl w-full hover:bg-primary-100 hover:italic hover:text-secondary [&.active]:font-normal [&.active]:border-secondary [&.active]:text-secondary [&.active]:text-2xl fill-inactiveText [&.active]:fill-secondary',
 
-        {{-- Création de variable de class pour éviter les copier coller avec AlpineJS --}}
-        <div
-            class="w-[80%] flex border rounded-xl overflow-hidden bg-slate-50"
-            x-data="{
-                initButtonClass: 'border-b border-slate-300 h-[50px] flex items-center text-left text-xl w-full pl-6 bg-slate-200 hover:bg-slate-100 [&.active]:bg-slate-50'
-            }">
+        }">
 
-            {{-- Pseudo navbar pour afficher tel ou tel onglet, 100% AlpineJS --}}
-            <div class="w-[350px] border-r flex-col flex">
-                <a  href="{{ route('dashboarduserdetails.index') }}" class="{{ Route::is('dashboarduserdetails.index') ? 'active' : ''}}"
-                    :class="initButtonClass">Détails du compte</a>
+        {{-- Pseudo navbar pour afficher tel ou tel onglet, 100% AlpineJS --}}
+        <div class="w-[375px] font-light flex-col flex">
 
-                <a href="#" :class="initButtonClass">Mes Skins</a>
+            <a  href="{{ route('dashboarduserdetails.index') }}" class="{{ Route::is('dashboarduserdetails.index') ? 'active' : ''}}"
+                :class="initButtonClass">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                    <path fill-rule="evenodd" d="M11.828 2.25c-.916 0-1.699.663-1.85 1.567l-.091.549a.798.798 0 01-.517.608 7.45 7.45 0 00-.478.198.798.798 0 01-.796-.064l-.453-.324a1.875 1.875 0 00-2.416.2l-.243.243a1.875 1.875 0 00-.2 2.416l.324.453a.798.798 0 01.064.796 7.448 7.448 0 00-.198.478.798.798 0 01-.608.517l-.55.092a1.875 1.875 0 00-1.566 1.849v.344c0 .916.663 1.699 1.567 1.85l.549.091c.281.047.508.25.608.517.06.162.127.321.198.478a.798.798 0 01-.064.796l-.324.453a1.875 1.875 0 00.2 2.416l.243.243c.648.648 1.67.733 2.416.2l.453-.324a.798.798 0 01.796-.064c.157.071.316.137.478.198.267.1.47.327.517.608l.092.55c.15.903.932 1.566 1.849 1.566h.344c.916 0 1.699-.663 1.85-1.567l.091-.549a.798.798 0 01.517-.608 7.52 7.52 0 00.478-.198.798.798 0 01.796.064l.453.324a1.875 1.875 0 002.416-.2l.243-.243c.648-.648.733-1.67.2-2.416l-.324-.453a.798.798 0 01-.064-.796c.071-.157.137-.316.198-.478.1-.267.327-.47.608-.517l.55-.091a1.875 1.875 0 001.566-1.85v-.344c0-.916-.663-1.699-1.567-1.85l-.549-.091a.798.798 0 01-.608-.517 7.507 7.507 0 00-.198-.478.798.798 0 01.064-.796l.324-.453a1.875 1.875 0 00-.2-2.416l-.243-.243a1.875 1.875 0 00-2.416-.2l-.453.324a.798.798 0 01-.796.064 7.462 7.462 0 00-.478-.198.798.798 0 01-.517-.608l-.091-.55a1.875 1.875 0 00-1.85-1.566h-.344zM12 15.75a3.75 3.75 0 100-7.5 3.75 3.75 0 000 7.5z" clip-rule="evenodd" />
+                </svg>
 
-                <a href="#" :class="initButtonClass">Mes Likes</a>
 
-                {{-- Onglet visible uniquement par les modérateurs et les admins --}}
-                @can('validate-skin')
-                    <a href="{{ route('pending-skins') }}" class="{{ Route::is('pending-skins') ? 'active' : ''}}"
-                       :class="initButtonClass">Skins en attente</a>
-                @endcan
+                <p>Détails du compte</p>
+            </a>
 
-                {{-- Onglet visible uniquement par les admins --}}
-                @can('admin-access')
-                    <a href="#" :class="initButtonClass">Liste des utilisateurs</a>
+            <a  href="#"
+                :class="initButtonClass">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                    <path d="M11.25 4.533A9.707 9.707 0 006 3a9.735 9.735 0 00-3.25.555.75.75 0 00-.5.707v14.25a.75.75 0 001 .707A8.237 8.237 0 016 18.75c1.995 0 3.823.707 5.25 1.886V4.533zM12.75 20.636A8.214 8.214 0 0118 18.75c.966 0 1.89.166 2.75.47a.75.75 0 001-.708V4.262a.75.75 0 00-.5-.707A9.735 9.735 0 0018 3a9.707 9.707 0 00-5.25 1.533v16.103z" />
+                </svg>
 
-                    <a  href="{{ route('adminpanel') }}" class="{{ Route::is('adminpanel') ? 'active' : ''}}"
-                        :class="initButtonClass">Panel Administrateur</a>
-                @endcan
-            </div>
 
-            <div class="w-full min-h-[350px]">
+                <p>Mes Skins</p>
+            </a>
 
-                {{ $slot }}
+            <a  href="#"
+                :class="initButtonClass">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                    <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+                </svg>
 
-            </div>
+                <p>Mes Likes</p>
+            </a>
+
+            {{-- Onglet visible uniquement par les modérateurs et les admins --}}
+            @can('validate-skin')
+                <a  href="{{ route('pending-skins') }}" class="{{ Route::is('pending-skins') ? 'active' : ''}}"
+                    :class="initButtonClass">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                        <path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
+                        <path fill-rule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z" clip-rule="evenodd" />
+                    </svg>
+
+                    <p>Skins en attente</p>
+                </a>
+            @endcan
+
+            {{-- Onglet visible uniquement par les admins --}}
+            @can('admin-access')
+
+                <a  href="#"
+                    :class="initButtonClass">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                        <path d="M4.5 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM14.25 8.625a3.375 3.375 0 116.75 0 3.375 3.375 0 01-6.75 0zM1.5 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM17.25 19.128l-.001.144a2.25 2.25 0 01-.233.96 10.088 10.088 0 005.06-1.01.75.75 0 00.42-.643 4.875 4.875 0 00-6.957-4.611 8.586 8.586 0 011.71 5.157v.003z" />
+                    </svg>
+
+
+                    <p>Liste des utilisateurs</p>
+                </a>
+
+
+                <a  href="{{ route('adminpanel') }}" class="{{ Route::is('adminpanel') ? 'active' : ''}}"
+                    :class="initButtonClass">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                        <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32L19.513 8.2z" />
+                    </svg>
+                        <p>Panel administrateur</p>
+                </a>
+            @endcan
+        </div>
+
+        <div class="w-full min-h-[50vh] relative">
+
+            {{ $slot }}
+
         </div>
     </div>
 </div>
