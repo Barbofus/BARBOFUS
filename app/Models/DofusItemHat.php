2be7c1731c2ca9f5c1cc33308a1981ac9e4f602a
CharlyMollard <charly.mollard@gmail.com>
2023-07-09 16:26:21 +0200

    Laravel pint

--- app/Models/DofusItemHat.php
+++ app/Models/DofusItemHat.php
@@ -2,12 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class DofusItemHat extends Model
 {
-
     protected $fillable = [
         'name',
         'dofus_id',
