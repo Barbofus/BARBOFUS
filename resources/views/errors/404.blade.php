fd20d12155400f323b73b0eee102130d9c0964d1
CharlyMollard <charly.mollard@gmail.com>
2023-07-13 16:57:11 +0200

    custom error pages

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.basic-views')
+
+@section('content')
+    <h1 class="text-[min(4rem,15vw)] mt-10 font-normal text-center uppercase">Erreur</h1>
+    <h2 class="text-2xl font-thin text-center -mt-3 mb-8 uppercase">404 - Page introuvable</h2>
+@endsection
