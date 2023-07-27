0a25178ae3d522b05ffdf60a7d905044e1c2c28e
CharlyMollard <charly.mollard@gmail.com>
2023-07-27 11:59:00 +0200

    changement logo mail

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="data:image/png;base64,{{ base64_encode(file_get_contents(public_path('storage/images/misc_ui/Barbofus_Logo_Full.png'))) }}" class="logo" alt="{{ $slot }}">
+<img src="data:image/png;base64,{{ base64_encode(file_get_contents(public_path('storage/images/misc_ui/Barbofus_Logo_Full.png'))) }}" class="header-logo" alt="{{ $slot }}">
 </a>
 </td>
 </tr>
