18e85e7fd77feccc75a17c9040b57c52e710ffe7
CharlyMollard <charly.mollard@gmail.com>
2025-05-16 12:12:16 +0200

    Ajout whitelist skinator

--- app/Http/Controllers/SkinatorController.php
+++ app/Http/Controllers/SkinatorController.php
@@ -20,8 +20,8 @@ class SkinatorController extends Controller
 
     public function create(Request $request): View
     {
-        // 2496 Coatox ; 8741 Yoroi ; 465 Gannon ; 9322 Kira ; 3230 Mcdonald
-        if (! Gate::check('mod-access') & ! Gate::check('admin-access') & ! in_array(auth()->id(), [2496, 8741, 465, 9322, 3230])) {
+        // 2496 Coatox ; 8741 Yoroi ; 465 Gannon ; 9322 Kira ; 3230 Mcdonald ; 10049 Zyros
+        if (! Gate::check('mod-access') & ! Gate::check('admin-access') & ! in_array(auth()->id(), [2496, 8741, 465, 9322, 3230, 10049])) {
             abort(403);
         }
 
@@ -76,8 +76,8 @@ class SkinatorController extends Controller
 
     public function edit(UnitySkin $skin): View
     {
-        // 2496 Coatox ; 8741 Yoroi ; 465 Gannon ; 9322 Kira ; 3230 Mcdonald
-        if (! Gate::check('mod-access') & ! Gate::check('admin-access') & ! in_array(auth()->id(), [2496, 8741, 465, 9322, 3230])) {
+        // 2496 Coatox ; 8741 Yoroi ; 465 Gannon ; 9322 Kira ; 3230 Mcdonald ; 10049 Zyros
+        if (! Gate::check('mod-access') & ! Gate::check('admin-access') & ! in_array(auth()->id(), [2496, 8741, 465, 9322, 3230, 10049])) {
             abort(403);
         }
 
