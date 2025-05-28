eb2ff19cb7350101921a8dc142f7109145405147
CharlyMollard <charly.mollard@gmail.com>
2025-05-28 18:37:16 +0200

    Correction de légers bugs

--- app/Actions/ItemsUpdate/uploadToFtp.php
+++ app/Actions/ItemsUpdate/uploadToFtp.php
@@ -13,9 +13,9 @@ final class uploadToFtp
     public function __invoke(array $localFiles, string $remoteDestination)
     {
         // Détails de la connexion FTP
-        $ftp_server = 'ftp.lema1810.odns.fr';  // Adresse du serveur FTP
-        $ftp_user_name = 'lema1810@barbofus.com';      // Nom d'utilisateur FTP
-        $ftp_user_pass = '6Tcn-meGp-mLb@';      // Mot de passe FTP
+        $ftp_server = env('FTP_SERVER_HOST');  // Adresse du serveur FTP
+        $ftp_user_name = env('FTP_SERVER_USER');      // Nom d'utilisateur FTP
+        $ftp_user_pass = env('FTP_SERVER_PASSWORD');      // Mot de passe FTP
 
         // Connexion au serveur FTP
         $ftp_conn = ftp_connect($ftp_server) or exit('Impossible de se connecter au serveur FTP');
